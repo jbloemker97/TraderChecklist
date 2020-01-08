717d68fb185d3cf969ab05f53c4083857f1a7a28
Jacob Bloemker <jbloemker97@gmail.com>
2020-01-07 19:49:18 -0500

    Init commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
