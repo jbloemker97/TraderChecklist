bb88365e044d044f76c85d743f8ecfae9311f9b9
Jacob Bloemker <jbloemker97@gmail.com>
2020-01-07 20:39:47 -0500

    Added db

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+const mongodb = require('mongodb');
+const { mongoURI } = require('../../config/dev');
+
+async function makeDb (dbName, mongoUri) {
+    const MongoClient = mongodb.MongoClient;
+    const client = new MongoClient(mongoURI, { useNewUrlParser: true });
+
+    await client.connect();
+
+    const db = await client.db(dbName);
+    
+    return db
+}
--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
+app.use(bodyParser.json());
+
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
